d222876f75e6d9defba76431fb40f3e747f16646
willy <shen.willy95@gmail.com>
2019-07-09 00:22:31 +0700

    feat: notification box UI

--- src/components/SiteHeader.svelte
+++ src/components/SiteHeader.svelte
@@ -1,8 +1,48 @@
 <script>
 	import { arweave, is_connected } from '../arweave.js';
+
+	let showNotifications = false;
+	function toggleNotifications(event) {
+		showNotifications = !showNotifications;
+	}
 </script>
 
-<div class="container">
+<style>
+	.notification-card {
+		margin: 3px;
+	}
+
+	.notification-box {
+		padding-top: 30px;
+		/* border: 1px solid #a5d6a7;  */
+		border-radius: 3px;
+		width: 30vw;
+		position: absolute;
+		right: 0; 
+		top: 0; 
+		z-index: -100;
+	}
+
+	@media (max-width: 991.98px) {
+		.notification-box {
+			width: 40vw;
+		}
+		
+	}
+	@media (max-width: 767.98px) {
+		.notification-box {
+			width: 55vw;
+		}
+	}
+	@media (max-width: 575.98px) {
+		.notification-box {
+			width: 100vw;
+		}
+	}
+
+</style>
+
+<div class="container" style="position: relative; z-index: 200">
 	{#if $is_connected === false}
 		<div class="row bg-dark-accent white py-1" style="border-radius: 0 0 10px 10px;">
 			<div class="col py-1 d-none d-sm-block">
@@ -28,9 +68,29 @@
 				Connected
 			</div>
 			<div class="px-2 ml-auto">
-				<i class="fa fa-bell clickable mx-2" aria-hidden="true"></i>
+				<i class="fa fa-bell clickable mx-2" aria-hidden="true" on:click={toggleNotifications}></i>
 				<!-- <span class="white-border clickable ml-2">My Family Tree</span> -->
 			</div>
 		</div>
+	{/if}
+	{#if showNotifications === true}
+	<div class="notification-box bg-dark-accent">
+		<div class="card notification-card">
+			<div class="card-body">
+				This is some text within a card body.
+			</div>
+		</div>
+
+		<div class="card notification-card">
+			<div class="card-body">
+				This is some text within a card body.
+			</div>
+		</div>
+		<div class="card notification-card">
+			<div class="card-body">
+				This is some text within a card body.
+			</div>
+		</div>
+	</div>
 	{/if}
 </div>
